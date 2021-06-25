9d086e1aa867ff1d127df3887df99847e7c1d870
Santosh Suresh <santosh.suresh@syncron.com>
2021-06-25 09:37:19 +0530

    Upgrade to latest version

--- server/src/notes/note.ts
+++ server/src/notes/note.ts
@@ -1,37 +1,37 @@
 import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  ManyToOne,
-  Index,
-  CreateDateColumn,
-  UpdateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToOne,
+    Index,
+    CreateDateColumn,
+    UpdateDateColumn,
 } from "typeorm";
 import { User } from "../account/user";
 
 @Entity()
 export class Note {
-  @PrimaryGeneratedColumn("uuid")
-  id: string;
-  @Column()
-  note: string;
-  @Column("text", { array: true, default: "{}" })
-  tags: string[];
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+    @Column()
+    note: string;
+    @Column("text", { array: true, default: "{}" })
+    tags: string[];
 
-  @Column({ nullable: true })
-  userId: string;
+    @Column({ nullable: true })
+    userId: string;
 
-  @Index()
-  @ManyToOne((type) => User)
-  user: User;
+    @Index()
+    @ManyToOne((type) => User)
+    user: User;
 
-  @CreateDateColumn({ name: "created_at" })
-  createdAt: Date;
+    @CreateDateColumn({ name: "created_at" })
+    createdAt: Date;
 
-  @UpdateDateColumn({ name: "updated_at" })
-  updatedAt: Date;
+    @UpdateDateColumn({ name: "updated_at" })
+    updatedAt: Date;
 }
 
 export interface Notes {
-  [key: string]: Note;
+    [key: string]: Note;
 }
